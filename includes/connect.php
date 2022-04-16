cededa772a2489cbc3ab5b89442bccc2c3ddeed4
sourasankar <64581748+sourasankar@users.noreply.github.com>
2022-04-16 22:13:32 +0530

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-// $servername = "194.163.35.151";
-// $server_user = "u708245423_5Wpp4";
-// $server_pass = "Kootty123#";
-// $dbname = "u708245423_eNgPt";
 $servername = "localhost";
 $server_user = "root";
 $server_pass = "";
