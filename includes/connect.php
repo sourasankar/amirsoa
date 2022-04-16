732c0122fa73b945284fe70c020543ceb84d59d2
sourasankar <64581748+sourasankar@users.noreply.github.com>
2022-04-16 11:04:36 +0530

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
-$servername = "194.163.35.151";
-$server_user = "u708245423_5Wpp4";
-$server_pass = "Kootty123#";
-$dbname = "u708245423_eNgPt";
+// $servername = "194.163.35.151";
+// $server_user = "u708245423_5Wpp4";
+// $server_pass = "Kootty123#";
+// $dbname = "u708245423_eNgPt";
+$servername = "localhost";
+$server_user = "root";
+$server_pass = "";
+$dbname = "food";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
